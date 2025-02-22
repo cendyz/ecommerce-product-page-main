4e4239e2ebb545d6aecc5a5b19be391b675538ae
cendyz <arek57823@gmail.com>
2025-02-22 21:44:05 +0100

    Mobile carousel

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,13 @@
 <template>
 	<Nav></Nav>
-	<main>
-		<!-- <VueSlickCarousel v-bind="settings">
-			<div><img src="https://picsum.photos/300/300" /></div>
-		</VueSlickCarousel> -->
-	</main>
+	<Header></Header>
+	<main></main>
+	<!-- <svg width="12" height="18" xmlns="http://www.w3.org/2000/svg"><path d="M11 1 3 9l8 8" stroke="#1D2026" stroke-width="3" fill="none" fill-rule="evenodd"/></svg> -->
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
-import 'vue-slick-carousel/dist/vue-slick-carousel.css'
+import Header from './components/Header.vue'
 import Nav from './components/Nav.vue'
-
-const settings = ref({
-	lazyLoad: 'ondemand',
-	dots: true,
-	fade: true,
-	infinite: true,
-	speed: 500,
-	slidesToShow: 1,
-	slidesToScroll: 1,
-})
 </script>
 
 <style lang="scss">
---
+++ src/components/Header.vue
@@ -0,0 +1,44 @@
+<template>
+	<header>
+		<Splide
+			:options="{
+				type: 'loop',
+				perPage: 1,
+				interval: 2500,
+				rewind: true,
+			}"
+			aria-label="My Favorite Images">
+			<SplideSlide v-for="(item, index) in imgData" :key="index">
+				<img :src="item" class="w-full" />
+			</SplideSlide>
+		</Splide>
+	</header>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import { Splide, SplideSlide } from '@splidejs/vue-splide'
+import '@splidejs/vue-splide/css'
+import product1 from '@/images/image-product-1.jpg'
+import product2 from '@/images/image-product-2.jpg'
+import product3 from '@/images/image-product-3.jpg'
+import product4 from '@/images/image-product-4.jpg'
+
+const imgData = ref([product1, product2, product3, product4])
+</script>
+
+<style lang="scss">
+.splide__arrow {
+	background-color: white;
+	width: 4.5rem;
+	height: 4.5rem;
+	opacity: 1;
+
+	svg {
+		width: 1.2rem;
+		height: 1.8rem;
+		stroke: #000000;
+		stroke-width: 3;
+	}
+}
+</style>
